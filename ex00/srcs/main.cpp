0f4bd00a017884f62b966863d35ef1b67f9c7ecd
pdvesa <saku.vesa@gmail.com>
2024-08-26 19:37:29 +0300

    i think i forgot something

---
+++ ex00/srcs/main.cpp
@@ -0,0 +1,9 @@
+#include <ClapTrap.hpp>
+
+int main() {
+    ClapTrap mrBOT("GPT");
+    mrBOT.attack("Wild Monke");
+    mrBOT.takeDamage(9);
+    mrBOT.beRepaired(7);
+    mrBOT.takeDamage(10);
+}
